9eca7f13e770ed6bf07bd0196f6748b47341fe48
Guruswamy-CK <chinna-krishnaia.gari-guruswamy@capgemini.com>
2024-10-16 16:55:35 +0530

    Test1
    Signed-off-by: Guruswamy-CK <chinna-krishnaia.gari-guruswamy@capgemini.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,25 @@ import { Router } from '@angular/router';
 import { CommonStorageService } from './services/common-storage/common-storage.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 
 export class AppComponent implements OnInit {
+	title: string = 'sap-sf-rpa-client';
 
-  constructor(@Inject(DOCUMENT) private document: Document, private router: Router, private commonStorageService: CommonStorageService) {
-    const origin = this.document.location.origin;
-    this.commonStorageService.baseUrl = origin;
-  }
-  ngOnInit(): void {
-    let urlRefreshed = this.document.location.hash.substring(2, this.document.location.hash.length);
-    if (urlRefreshed) {
-      this.router.navigate([urlRefreshed]);
-    } else {
-      this.router.navigate(['login']);
-    }
+	constructor(@Inject(DOCUMENT) private document: Document, private router: Router, private commonStorageService: CommonStorageService) {
+		const origin = this.document.location.origin;
+		this.commonStorageService.baseUrl = origin;
+	}
+	ngOnInit(): void {
+		let urlRefreshed = this.document.location.hash.substring(2, this.document.location.hash.length);
+		if (urlRefreshed) {
+			this.router.navigate([urlRefreshed]);
+		} else {
+			this.router.navigate(['login']);
+		}
 
-  }
+	}
 }
